9ce8ac4040caff5d103a082d478e97ebe8821a08
asim <asimneupane11@gmail.com>
2025-11-02 21:55:18 +0545

    setup:initial-setup

---
+++ src/config/config.js
@@ -0,0 +1,38 @@
+// import {dbConfig} from './index'
+
+// export const config={
+//   development: {
+//     username: dbConfig.DB_USER,
+//     password: dbConfig.DB_PASSWORD,
+//     database: dbConfig.DB_NAME,
+//     host: dbConfig.DB_HOST,
+//     dialect: "mysql"
+//   },
+//   test: {
+//     username: "root",
+//     password: null,
+//     database: "mydb",
+//     host:"127.0.0.1",
+//     dialect: "mysql"
+//   },
+//   production: {
+//     username: "root",
+//     password: null,
+//     database: "mydb",
+//     host: "127.0.0.1",
+//     dialect: "mysql"
+//   }
+// }
+import dotenv from 'dotenv'
+dotenv.config()
+const config = {
+    development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  },
+}
+
+export default config
